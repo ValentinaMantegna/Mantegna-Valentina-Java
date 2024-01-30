e8b9107efdbf583f9e5ac7f2dbb0acc4c3339b5c
ValentinaMantegna <v.alky@hotmail.it>
2024-01-30 16:40:45 +0100

    messaggio

---
+++ Lezione1/EsercizioInput.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class  EsercizioInput {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in); // deve esserci sempre per il terminale
+        System.out.print("Inserisci un nome: "); //togliere ln per non andare a capo;lbn significa nuova linea
+        System.out.print("Inserisci un cognome: "); 
+        System.out.print("Inserisci eta: ");
+        System.out.print("Inserisci base: ");
+        System.out.print("Inserisci altezza: ");
+        String nome = tastiera.nextLine(); //variabile con il nome tastiera
+        String cognome = tastiera.nextLine();
+        String numero= "4";
+        int eta= Integer.parseInt(tastiera.nextLine()); // coversione di un tipo di variabile
+        int base= Integer.parseInt(tastiera.nextLine());
+         int altezza= Integer.parseInt(tastiera.nextLine());
+       // System.out.println("Ciao "+ nome);
+        System.out.println("Ciao "+ nome +" "+ cognome +" e hai "+ eta*2+ " anni " + " Area: "+ base*altezza);
+       
+        int a = Integer.parseInt(numero);
+
+        int b =0;
+        System.out.println(b);
+        double n1 = (double)a;
+        char c = "A"; // singoli caratteri corrispondono a un numero (ASCII)
+        int n2 = (int)c;
+        System.out.println(n2);
+
+        //a++ stampato 4 e incrementa e mette 5
+        //++a incrementa e poi stampa 5
+       
+
+
+    }
+}
---
+++ Lezione1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        int a = 4;
+        int b =a+2;                                                                                               
+        System.out.println(a); //creata una cella chiamata a con 4 
+        double c = 4.2; //numeri virgola; è un tipo primitivo pag 56 del libro
+        String nome = "Ciao";
+
+    }
+
+}
