549672a8ddcddad82639b07e51c20748b34710e5
ValentinaMantegna <v.alky@hotmail.it>
2024-02-06 16:39:13 +0100

    uu

--- Lezioni/src/lezione2/riscaldamento/Boolean.java
+++ Lezioni/src/lezione2/riscaldamento/Boolean.java
@@ -1,4 +1,14 @@
 package lezione2.riscaldamento;
 
+import java.util.Scanner;
+//chiedere all'utente un numero
+// stampare true se un voto insufficiente (cioè tra 0 e 5)
 public class Boolean {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Inserisci numero: ");
+        int voto = Integer.parseInt(tastiera.nextLine());
+        boolean condizione = (voto > 0) && (voto <= 5) ;
+        System.out.println(condizione);
+    }
 }
--- Lezioni/src/lezione2/riscaldamento/EsercizioEquals.java
+++ Lezioni/src/lezione2/riscaldamento/EsercizioEquals.java
@@ -1,4 +1,21 @@
 package lezione2.riscaldamento;
 
+import java.util.Scanner;
+
 public class EsercizioEquals {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("utente: ");
+        String utente = tastiera.nextLine();
+        System.out.println("password: ");
+        String password = tastiera.nextLine();
+        boolean condizione = utente.equals("Luana") && password.equals("Maria");
+        System.out.println(condizione);
+        System.out.println(condizione);
+        if(condizione) {
+            System.out.println("Messaggio segreto");
+        } else {
+            System.out.println("User e/o password errati");
+        }
+    }
 }
